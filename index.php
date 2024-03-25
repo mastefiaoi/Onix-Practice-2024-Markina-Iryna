f684e51ef20cb55e5325fab6416e77db68d2c21d
Markina Iryna <mastefiaoi@gmail.com>
2024-03-25 21:40:42 +0200

    Created fields to receive information entered by the user

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "Hello, World!";
+echo "*************************************************************\n";
+echo "*   Fill in the information about the book you want to add  *\n";
+echo "*      Enter this information in the appropriate fields     *\n";
+echo "*            Do not leave input fields blank!               *\n";
+echo "*************************************************************\n\n";
+echo "Title:";
+$title = trim(fgets(STDIN));
+echo "Author: ";
+$autor = trim(fgets(STDIN));
+echo "Year of publication: ";
+$year = trim(fgets(STDIN));
+echo "Сommentary to the book: ";
+$commentary = trim(fgets(STDIN));
 ?>

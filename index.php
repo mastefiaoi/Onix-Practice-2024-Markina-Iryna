0d237ccd1dea345950cf403a470be31db6f712c8
Markina Iryna <mastefiaoi@gmail.com>
2024-03-26 00:10:34 +0200

    Added a code snippet to create a new file in a folder and entering data there

--- index.php
+++ index.php
@@ -4,12 +4,49 @@ echo "*   Fill in the information about the book you want to add  *\n";
 echo "*      Enter this information in the appropriate fields     *\n";
 echo "*            Do not leave input fields blank!               *\n";
 echo "*************************************************************\n\n";
+
 echo "Title:";
 $title = trim(fgets(STDIN));
+
 echo "Author: ";
-$autor = trim(fgets(STDIN));
+$author = trim(fgets(STDIN));
+
 echo "Year of publication: ";
 $year = trim(fgets(STDIN));
-echo "Сommentary to the book: ";
+
+echo "Commentary to the book: ";
 $commentary = trim(fgets(STDIN));
+
+$currentDirectory = getcwd();
+$outputDirectory = isset($argv[1]) ? $argv[1] : $currentDirectory;
+
+if (empty($fileNameInput)) {
+    $timestamp = date('YmdHis');
+    $fileName = sprintf(
+        "%s-%s-%s.md",
+        str_replace(' ', '-', strtolower($title)),
+        strtolower($author),
+        $year
+    );
+} else {
+    $fileName = $fileNameInput . ".md";
+}
+
+$filePath = $outputDirectory . DIRECTORY_SEPARATOR . $fileName;
+
+$content = <<<MD
+---
+Title: "$title"
+Author: "$author"
+Year of publication: "$year"
+Commentary to the book: "$commentary"
+---
+
+Your entered information is displayed...
+
+MD;
+
+file_put_contents($filePath, $content);
+
+echo "Information has been updated: $filePath\n";
 ?>
